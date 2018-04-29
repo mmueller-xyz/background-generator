7444f01826b3352012532f9c127ccecd5983e3a5
Maximilian Müller <maximilian@mmueller.xyz>
2018-04-29 03:16:49 +0200

    switched to multiprocessing

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from PIL import Image, ImageFilter
+from multiprocessing import Process
 import imagegenerator
 
 
-class BGCreator:
+class BGCreator(Process):
     def __init__(self, stencil_color_list =(
             '#1c1d1c', '#1f2f3f',
             '#1a7461', '#00a169',
@@ -16,14 +17,14 @@ class BGCreator:
             '#5ec5ed', '#99d3db',
             '#923f3f',
             '#4b4643', '#ededed'),
-             scale_factor = 10,
+             scale_factor = 1/10,
              logo_filename="castle_black.png",
              prefix="out/castle",
              suffix=".png",
              x_offset=1/4,
              y_offset=1/2,
              img_size=(2560, 1440)):
-
+        Process.__init__(self)
         self.stencil_color_list = stencil_color_list
         self.bg_color_list = bg_color_list
         self.scale_factor = scale_factor
@@ -34,15 +35,16 @@ class BGCreator:
         self.y_offset = y_offset
         self.img_size = img_size
 
-    def start(self):
+    def run(self):
         # open the staple
         staple = Image.open(self.logo_filename)
         # Blur the mask
-        staple = staple.filter(ImageFilter.GaussianBlur(4))
+        if self.scale_factor < .5:
+            staple = staple.filter(ImageFilter.GaussianBlur(4))
         # get the staple size
         staple_size = staple.size
         # resize the staple
-        staple = staple.resize((int(staple_size[0] / self.scale_factor), int(staple_size[1] / self.scale_factor)))
+        staple = staple.resize((int(staple_size[0] * self.scale_factor), int(staple_size[1] * self.scale_factor)))
         # sharpen the mask
         # staple = staple.filter(ImageFilter.SHARPEN)
         # get the staple size
@@ -72,4 +74,22 @@ class BGCreator:
 
 
 if __name__ == "__main__":
-    BGCreator().start()
+    # castle
+    BGCreator(stencil_color_list =(
+            '#1c1d1c', '#1f2f3f',
+            '#1a7461', '#00a169',
+            '#88c591', '#ffde85',
+            '#ffe169', '#f8ae61',
+            '#ea573c', '#d6174a',
+            '#923f3f', '#4b4643',
+            '#ededed'), bg_color_list=(
+            '#1c1d1c', '#1f2f3f',
+            '#99d3db', '#923f3f',
+            '#4b4643', '#ededed'),
+             scale_factor=1 / 10,
+             logo_filename="castle_black.png",
+             prefix="out/castle",
+             suffix=".png",
+             x_offset=1/4,
+             y_offset=1/2,
+             img_size=(2560, 1440)).start()
